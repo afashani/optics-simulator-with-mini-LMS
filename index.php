4152efd95904ea1be421d1e791eea6b8c982c02d
Ashani Fonseka <52863004+afashani@users.noreply.github.com>
2021-12-19 17:26:02 +0530

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="../assets/css/main.css"> 
+<link rel="stylesheet" href="assets/css/main.css"> 
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
