350d0d0d3ae5092ac1fe3c7ab799248599dc8306
amitha-priyangi <81142542+amitha-priyangi@users.noreply.github.com>
2022-03-01 22:06:47 +0530

    simulator-fontend

---
+++ pages/simulator.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<head>
+    <title>Optics Simulator</title>
+    <style> 
+    main{
+        position: relative;
+    }
+        canvas#defaultCanvas0{
+            position: relative;
+            width: 100% !important;
+            height: calc(100vh - 100px) !important;
+            padding: 0 5%;
+        }
+    </style>
+</head>
+<body id="bg">
+
+    <script src="../include/p5.js" type="text/javascript"></script>
+    <script src="../include/simulator.js" type="text/javascript"></script>
+    <?php include '../include/header2.html' ?> 
+    
+    <main>
+    </main>   
+    
+    
+    <div class="container">
+    <div class="details">
+        <h1>
+            test
+        </h1>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+    </div>
+   
+    <!-- Footer ==== -->
+        <footer>
+		<?php include '../include/footer.html' ?> 
+		
+	</footer>
+    <!-- Footer END ==== -->
+</body>
+</html>
